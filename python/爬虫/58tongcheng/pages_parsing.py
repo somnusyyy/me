2f08308ee7c7e8bc61f6cff9fdbf5131b0b4b4aa
somnusyyy <47393005+somnusyyy@users.noreply.github.com>
2019-07-26 23:24:17 +0800

    Update pages_parsing.py

--- python/爬虫/58tongcheng/pages_parsing.py
+++ python/爬虫/58tongcheng/pages_parsing.py
@@ -19,6 +19,7 @@ def get_links_from(channel,page,who_sell=0):
                 for link in soup.select('td.t a.t'):
                         item_link = link.get('href').split('?')[0]
                         url_list.insert_one({'url':item_link})
+                        # return item_link
                         print(item_link)
         else:
                 pass
@@ -28,16 +29,24 @@ def get_links_from(channel,page,who_sell=0):
 def get_item_info(url):
         web_data = requests.get(url)
         soup = BeautifulSoup(web_data.text, 'lxml')
-        title = soup.title.text
-        price=soup.select('div.infocard__container__item__main > span')[0].text.strip()
-        date = soup.select('div.detail-title__info > div:nth-child(1)')[0].text.strip()
-        # area = list(soup.select('div.infocard__container__item__main > a')[0].stripped_strings) if soup.find_all('span', 'c_25d') else None
-        item_info.insert_one({"title": title, "price": price, "date":date, 'url': url})
-        print({"title": title, "price": price, "date": date, 'url': url})
+        no_longer_exist = '404' in soup.find('script', type="text/javascript").get('src').split('/')
+        if no_longer_exist:
+                pass
+        else:
+                title = soup.title.text
+                price=soup.select('div.infocard__container__item__main > span')[0].text.strip()
+                date = soup.select('div.detail-title__info > div:nth-child(1)')[0].text.strip()
+                # area = list(soup.select('div.infocard__container__item__main > a')[0].stripped_strings) if soup.find_all('span', 'c_25d') else None
+                item_info.insert_one({"title": title, "price": price, "date":date, 'url': url})
+                print({"title": title, "price": price, "date": date, 'url': url})
         
         
+#get_item_info('https://bj.58.com/shouji/pn5')
 
-#get_links_from('https://bj.58.com/shouji/',5)
+# for page_num in range(1, 50):
+#         url_link = 'https://bj.58.com/shouji/pn{}'.format(str(page_num))
+#         print(url_link)
+#         get_item_info(url_link)
 
 # get_item_info('https://bj.58.com/shouji/38906156683048x.shtml?link_abtest=&psid=191980321205002019964782033&entinfo=38906156683048_p&slot=-1')
 # get_item_info('https://bj.58.com/huishou/38703741878047x.shtml?adtype=1&adact=3&psid=116742417205002763965142277&iuType=b_1&link_abtest=&ClickID=5&PGTID=0d300024-0000-11ee-7cb0-3b96abcda5a8&slot=1000106')
