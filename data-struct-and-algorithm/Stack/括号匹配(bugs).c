0431772a307ec943e097c77f02267d3d766ffc4e
somnusyyy <47393005+somnusyyy@users.noreply.github.com>
2019-10-05 00:37:07 +0800

    有错误

---
+++ data-struct-and-algorithm/Stack/括号匹配(bugs).c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char ElementType;
+
+typedef struct Node{
+    ElementType Element;
+    struct Node * Next;
+}NODE,*PNODE;
+typedef PNODE stack;
+
+int isEmpty(stack S){
+    return S->Next==NULL;
+}
+
+
+stack createStack(void){
+    stack S;
+    S=(stack)malloc(sizeof(NODE));
+    if(NULL!=S){
+        S->Next=NULL;
+    }
+    return S;
+}
+
+ElementType top(stack S){
+//    if(isEmpty(S)){
+////        printf("it works.\n");
+//        return;
+//    }
+    ElementType temp;
+    S=S->Next;
+    temp=S->Element;
+    return temp;
+}
+
+void pop(stack S)
+{
+	if(isEmpty(S))
+	{
+		perror("Stack is empty");
+		return 0;
+	}
+	PNODE P;
+	P = S->Next;
+	S->Next = P->Next;
+	free(P);
+}
+
+void push(ElementType x,stack S){
+    stack temp;
+    temp=(stack)malloc(sizeof(NODE));
+    if(NULL==temp){
+        printf("Not Enough memory.\n");
+        return;
+    }
+    temp->Element=x;
+    temp->Next=S->Next;
+    S->Next=temp;
+
+}
+
+int match(char str,char temp){
+    if((str=='('&&temp==')')||(str=='['&&temp==']')||(str=='{'&&temp=='}'))
+        return 1;
+    else return 0;
+}
+
+int main()
+{
+    stack S;
+    S=(stack)malloc(sizeof(NODE));
+    if(NULL==S) printf("out of space.\n");
+    S->Next=NULL;
+    char str[100];
+    scanf("%s",str);
+    for(int i=0;i<strlen(str);i++){
+        if(str[i]=='('||str[i]=='['||str[i]=='{')
+            push(str[i],S);
+        else {
+//            if(isEmpty(S)){
+//                printf("fail.\n");
+//                return;
+//            }
+            char temp=top(S);
+//            printf("%c",temp);
+            if(match(str[i],temp)){
+                printf("%c,%c\n",str[i],temp);
+                pop(S);
+            }
+
+
+        }
+    }
+//    printf("%d\n",isEmpty(S));
+    if(!isEmpty(S)) printf("success.\n");
+    else printf("fail.\n");
+    return 0;
+}
