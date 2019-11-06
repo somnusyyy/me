05fdfb9bc5e0ee8b2ecb2ddf0af141f55c9025f4
e <1731248907@qq.com>
2019-11-06 19:35:08 +0800

    update fileStream

---
+++ javabegin/src/Thread/ArmyRunnable.java
@@ -0,0 +1,19 @@
+package Thread;
+
+public class ArmyRunnable implements Runnable {
+
+    volatile boolean keepRunning = true;
+
+    @Override
+    public void run() {
+        while (keepRunning) {
+            for(int i=0;i<5;i++){
+                System.out.println(Thread.currentThread().getName()+"发功了攻击["+i+"]");
+            }
+            Thread.yield();
+        }
+        System.out.println(Thread.currentThread().getName()+"结束了攻击");
+
+    }
+
+}
---
+++ javabegin/src/Thread/stage.java
@@ -0,0 +1,57 @@
+package Thread;
+
+public class stage extends Thread {
+
+    public void run(){
+        ArmyRunnable ArmyOfSuiDynasty=new ArmyRunnable();
+        ArmyRunnable ArmyOfRevolt=new ArmyRunnable();
+
+        Thread ArmySuiDynasty=new Thread(ArmyOfSuiDynasty,"隋军");
+        Thread ArmyRevolt=new Thread(ArmyOfRevolt,"起义军");
+
+        ArmySuiDynasty.start();
+        ArmyRevolt.start();
+
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ArmyOfSuiDynasty.keepRunning=false;
+        ArmyOfRevolt.keepRunning=false;
+
+        KeyPersonThread MrChengThread=new KeyPersonThread();
+
+        Thread MrCheng=new Thread(MrChengThread,"MrCheng");
+        MrCheng.start();
+
+        try {
+            MrCheng.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+//        MrChengThread.KeepRunning=false;
+
+
+
+//        try {
+//            Thread.sleep(50);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        System.out.println("演出结束");
+
+
+    }
+
+    public static void main(String[] args) {
+//        stage.currentThread().run();
+        new stage().run();
+    }
+
+
+
+}
